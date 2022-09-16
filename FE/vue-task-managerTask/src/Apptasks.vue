839acceb4557896e66ecb96903453b7da07fe39e
Amin Akhtar <Amin.Akhtar@TRULIOO.NET>
2022-09-15 19:19:04 -0700

    Add more of the FE code - data read

---
+++ FE/vue-task-managerTask/src/Apptasks.vue
@@ -0,0 +1,12 @@
+<script setup>
+import TaskComps from './components/TasksComp.vue'
+</script>
+
+<template>
+  <body>
+    <div class="wrapper">
+      <TaskComps />
+    </div>
+  </body>
+
+</template>
---
+++ FE/vue-task-managerTask/src/tasks.js
@@ -0,0 +1,8 @@
+import { createApp } from 'vue'
+import AppTasks from './Apptasks.vue'
+
+import './assets/main.css'
+
+createApp(AppTasks).mount('#apptasks')
+
+
